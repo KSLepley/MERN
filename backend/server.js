abe3f15116005e086cbe0cb4281c047a14848347
“Kaylie <klepley@ucsc.edu>
2025-01-14 09:04:42 -0800

    dnld app

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready");
-})
+app.get("/products", async (req, res) => {
+    const product = req.body; // user will send this date
+    if (!product.name || !product.price | !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields"});
+    }
 
-console.log(process.env.MONGO_URI);
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success: true, data: newProduct})
+    }   catch (error) {
+        console.error("Error in creating product:", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+});
+
+//console.log(process.env.MONGO_URI);
 
 app.listen(5001, () => {
+    connectDB();
     console.log('Server started at http://localhost:5001');
 });
 
